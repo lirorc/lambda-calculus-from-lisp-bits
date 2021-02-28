6b0ce0bcb7df51e25a182aca9d732cb6d99f3cd4
lirorc <74657909+lirorc@users.noreply.github.com>
2021-02-28 23:17:07 +0300

    Added eq? ge? substraction and comments

--- lambda.scm
+++ lambda.scm
@@ -3,17 +3,17 @@
 ;false
 (define yanlış (lambda (x) (lambda (y) y)))
 
-;not
+;not: (zıt doğru) -> yanlış
 (define zıt 
   (lambda (x) ((x yanlış) doğru)))
-;and
+;and: ((ve doğru) yanlış) -> yanlış
 (define ve
   (lambda (x) (lambda (y) ((x y) yanlış))))
-;or
+;or: ((veya yanlış) doğru) -> doğru
 (define veya
   (lambda (x) (lambda (y) ((x doğru) y))))
 
-;if
+;if: (((eğer doğru) bir) sıfır) -> bir
 (define eğer
   (lambda (x) (lambda (y) (lambda (z) ((x y) z)))))
 
@@ -27,25 +27,41 @@
 (define iki
   (lambda (f) (lambda (x) (f (f x)))))
 
-;addition
+;addition: ((iki ++) bir) -> 2+1 = 3
 (define ++
-  (lambda (y)
-    (lambda (f) (lambda (x) (f ((y f) x))))))
+  (lambda (y) (lambda (f)
+                (lambda (x) (f ((y f) x))))))
 
 ;is-zero?
 (define sıfır?
   (lambda (x) (((x yanlış) zıt) yanlış)))
 
-;make a pair
+;make a pair: ((liste iki) bir) -> [iki, bir]
 (define liste
   (lambda (x) (lambda (y)
                 (lambda (z) ((z x) y)))))
 
-;even?
+;even?: (çift-sayı? iki) -> doğru
 (define çift-sayı?
   (lambda (x) ((x zıt) doğru)))
 
-;(n, n2) -> (n+1, n)
+;(n, n2 -> n+1, n)
 (define fi
   (lambda (liste) 
     (lambda (z) ((z (++ (liste doğru))) (liste doğru)))))
+
+;predecessor: ((bir --) iki) -> 2-1 = 1
+(define --
+  (lambda (n)
+    (((n fi) ((liste sıfır) sıfır)) yanlış)))
+
+;greater-than-or-equal?: ((büyük-eşittir iki) bir) -> doğru
+(define büyük-eşittir?
+  (lambda (x) (lambda (y)
+                (sıfır? ((x --) y)))))
+
+;equal?: ((eşittir? iki) iki) -> doğru
+(define eşittir?
+  (lambda (x) (lambda (y)
+                ((ve ((büyük-eşittir? x) y))
+                     ((büyük-eşittir? y) x)))))
